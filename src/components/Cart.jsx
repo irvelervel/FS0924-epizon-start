715e099075ca32f12a2b739c1b51148a03df013f
Stefano Casasola <ste.casasola@gmail.com>
2025-01-20 13:07:57 +0100

    Finishes today's project

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,36 +1,53 @@
 import { Col, Row, Button } from 'react-bootstrap'
 import { FaTrash } from 'react-icons/fa'
+import { useDispatch, useSelector } from 'react-redux'
 
-const Cart = ({ cart = [] }) => (
-  <Row>
-    <Col sm={12}>
-      <ul style={{ listStyle: 'none' }}>
-        {cart.map((book, i) => (
-          <li key={i} className="my-4">
-            <Button variant="danger" onClick={() => {}}>
-              <FaTrash />
-            </Button>
-            <img
-              className="book-cover-small"
-              src={book.imageUrl}
-              alt="book selected"
-            />
-            {book.title}
-          </li>
-        ))}
-      </ul>
-    </Col>
+const Cart = () => {
+  const cart = useSelector((state) => {
+    return state.cart.content
+  })
+
+  const dispatch = useDispatch()
+
+  return (
     <Row>
-      <Col sm={12} className="fw-bold mb-3 ms-4">
-        TOTALE:{' '}
-        {cart.reduce(
-          (acc, currentValue) => acc + parseFloat(currentValue.price),
-          0
-        )}
-        $
+      <Col sm={12}>
+        <ul style={{ listStyle: 'none' }}>
+          {cart.map((book, i) => (
+            <li key={i} className="my-4">
+              <Button
+                variant="danger"
+                onClick={() => {
+                  dispatch({
+                    type: 'REMOVE_FROM_CART',
+                    payload: i,
+                  })
+                }}
+              >
+                <FaTrash />
+              </Button>
+              <img
+                className="book-cover-small"
+                src={book.imageUrl}
+                alt="book selected"
+              />
+              {book.title}
+            </li>
+          ))}
+        </ul>
       </Col>
+      <Row>
+        <Col sm={12} className="fw-bold mb-3 ms-4">
+          TOTALE:{' '}
+          {cart.reduce(
+            (acc, currentValue) => acc + parseFloat(currentValue.price),
+            0
+          )}
+          $
+        </Col>
+      </Row>
     </Row>
-  </Row>
-)
+  )
+}
 
 export default Cart
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -11,6 +11,11 @@ const initialState = {
   },
 }
 
+// il reducer deve SEMPRE essere una funzione PURA:
+// 1) dato lo stesso input, ritornerà sempre lo stesso output
+// 2) NON deve contattare l'internet o fare side-effects
+// 3) NON deve MAI mutare i propri parametri (eg. state, action)
+
 const mainReducer = (state = initialState, action) => {
   // questa funzione ritornerà SEMPRE il nuovo stato dell'applicativo
   // all'inizio, quando l'app si carica per la prima volta, creiamo noi
@@ -20,6 +25,51 @@ const mainReducer = (state = initialState, action) => {
   // in ogni reducer va implementato uno SWITCH CASE
   switch (action.type) {
     // qui andremo dopo a definire i vari "case"
+
+    case 'ADD_TO_CART':
+      return {
+        // questo nuovo oggetto che vado a creare sarà il NUOVO
+        // stato di Redux per l'applicativo!
+        ...state, // mi sto portando dentro questo nuovo oggetto ("guscio")
+        // tutte le proprietà dell'oggetto state
+        cart: {
+          ...state.cart,
+          // --> DANGER <-- (non si può fare in una funzione pura!)
+          // content: state.cart.content.push() // ?
+          content: state.cart.content.concat(action.payload),
+          // concat TORNA SEMPRE UN NUOVO ARRAY! non modifica quello esistente
+          // metodo alternativo
+          // content: [...state.cart.content, action.payload],
+        },
+      }
+
+    case 'REMOVE_FROM_CART':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          content: state.cart.content.filter((book, i) => {
+            //   // devo controllare ogni book: uno di essi NON deve far parte
+            //   // del nuovo array
+            if (action.payload === i) {
+              // ho trovato il libro da rimuovere!
+              // lo filtro via
+              return false
+            } else {
+              return true
+            }
+            //   // VERSIONE PRO
+            //   // return action.payload !== i
+          }),
+          // metodo alternativo (delle "due fette")
+          // content: [
+          //   ...state.cart.content.slice(0, action.payload),
+          //   ...state.cart.content.slice(action.payload + 1),
+          // ],
+          // },
+        },
+      }
+
     default:
       // default è la casistica che avviene quando NESSUNA delle precedenti
       // viene colpita
